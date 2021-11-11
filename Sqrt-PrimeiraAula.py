77c1924cfdb8606f8421bfb57ebbadaa867a435b
MarcosMello <Marcosvinicius-vm@outlook.com>
2021-11-11 09:06:21 -0300

    Changed SQRT function name

--- Sqrt-PrimeiraAula.py
+++ Sqrt-PrimeiraAula.py
@@ -7,12 +7,12 @@ def mod(x):
    else:
       return x * -1;
 
-def sqrtmine(x, i):
+def sqrtMV(x, i):
    if (mod(i**2 - x) < 0.000000000001):
       return i;
    else:
       i = (i+(x/i))/2;
-      return sqrtmine(x, i);
+      return sqrtMV(x, i);
 
-print(sqrtmine(x, i));
+print(sqrtMV(x, i));
 print(2**0.5);
