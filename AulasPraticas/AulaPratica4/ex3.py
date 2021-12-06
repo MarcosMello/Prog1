d341a054490099866caa268e20056f3cf562b1aa
MarcosMello <Marcosvinicius-vm@outlook.com>
2021-12-06 10:55:34 -0300

    Removed ";" from all the lines it was in.

--- AulasPraticas/AulaPratica4/ex3.py
+++ AulasPraticas/AulaPratica4/ex3.py
@@ -1,14 +1,14 @@
 def main():
-    s = int(input("Digite a quatidade de segundos: "));
-    converte(s);
+    s = int(input("Digite a quatidade de segundos: "))
+    converte(s)
 
 def converte(segundos):
-    horas = segundos//3600;
-    segundos -= horas*3600;
+    horas = segundos//3600
+    segundos -= horas*3600
 
-    minutos = segundos//60;
-    segundos -= minutos*60;
+    minutos = segundos//60
+    segundos -= minutos*60
 
-    print(f"{horas} : {minutos} : {segundos}");
+    print(f"{horas} : {minutos} : {segundos}")
 
-main();
+main()
--- AulasPraticas/AulaPratica4/ex4.py
+++ AulasPraticas/AulaPratica4/ex4.py
@@ -1,17 +1,17 @@
-from math import factorial as fac, comb as cb;
+from math import factorial as fac, comb as cb
 
 def main():
-    n = int(input("N: "));
-    p = int(input("P: "));
+    n = int(input("N: "))
+    p = int(input("P: "))
 
     if (p <= n):
-        comb(n, p); #minha funcao
-        #print(f"Cb = {cb(n, p)}."); #funcao de combinacao da lib math 
+        comb(n, p) #minha funcao
+        #print(f"Cb = {cb(n, p)}.") #funcao de combinacao da lib math 
     else:
-        print("Erro");
+        print("Erro")
 
 def comb(n, p):
-    c = fac(n) / (fac(n-p) * fac(p));
-    print(f"C = {c}.");
+    c = fac(n) / (fac(n-p) * fac(p))
+    print(f"C = {c}.")
 
-main();
+main()
