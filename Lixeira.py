c762d5f752c65e804e0becf54a868f5098caf0f3
MarcosMello <Marcosvinicius-vm@outlook.com>
2021-12-09 13:19:59 -0300

    Lixeira things

---
+++ Lixeira.py
@@ -0,0 +1,16 @@
+def rec():
+    x = int(input("Insira um valor valido: "))
+    if x < 0:
+        return(rec());
+    else:
+        return x
+
+def main():
+    x = int(input("Insira um valor: "))
+    if x < 0:
+        x = rec()
+        print(f"Agora sim Ok, {x}")
+    else:
+        print(f"Ok, {x}")
+
+main()
