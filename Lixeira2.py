5443a026340a5a2f343e2f3eafa099e4e5b4dcae
MarcosMello <Marcosvinicius-vm@outlook.com>
2022-01-15 14:39:38 -0300

    Updated Lixeira2.py with some code used on the examples from ApostilaRecursao (probably).

--- Lixeira2.py
+++ Lixeira2.py
@@ -1,6 +1,18 @@
-def a():
-    print(3)
-    return 3
+def f(n, x = 0):
+    if n == 2:
+        return x + 2
+    elif n % 2 == 0:
+        x += n;
+    
+    return f(n-1, x)
 
-a()
-print(3)
+def f1(n):
+    if n == 2:
+        return 2
+    elif n % 2 == 1:
+        return f(n-1)
+    else:
+        return n + f(n-1)
+
+print(f(10))
+print(f1(10))
