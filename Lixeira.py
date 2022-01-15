fbb42b1cc6715abfca9e761ea5c65513f4e207ab
MarcosMello <Marcosvinicius-vm@outlook.com>
2022-01-15 14:38:39 -0300

    Updated Lixeira.py with some code used on mEP4.py.

--- Lixeira.py
+++ Lixeira.py
@@ -1,87 +1,39 @@
-ok = True
-doc = "-"
-
-peso = float(input())
-if (peso < 50):
-    ok = False
 idade = int(input())
-if (16 > idade):
-    ok = False
-elif (idade < 18):
-    doc = input()
-    if (doc == "N"):
-        ok = False
-elif (idade > 69):
-    ok = False
-saude = input()
-if (saude == "N"):
-    ok = False
-drogas = input()
-if (drogas == "S"):
-    ok = False
-primeira = input()
-if (primeira == "S"):
-    if (idade > 60):
-        ok = False
-else:
-    meses = int(input())
-    ultimas = int(input())
-sexob = input()
 
-if (sexob == "F"):
-    gravida = input()
-    if (gravida == "S"):
-        ok = False
-    amamentando = input()
-    if (amamentando == "S"):
-       idadebb = int(input())
-       if (idadebb < 12):
-           ok = False
-    if ((primeira == "N") and (meses < 3) and (ultimas >= 3)):
-        ok = False
+if (idade <= 34): #aqui comeca +5
+    tMax = 180
+elif (idade <= 39):
+    tMax = 185 #+5
+elif (idade <= 44):
+    tMax = 190 #+5
+elif (idade <= 49): #aqui interrompe
+    tMax = 200 #+10 -> +5 habitual +5 que tem que ser a mais
+elif (idade <= 54):
+    tMax = 205 #+5
+elif (idade <= 59): #aqui comeca +15
+    tMax = 215 #+10
+elif (idade <= 64):
+    tMax = 230 #+15
+elif (idade <= 69):
+    tMax = 245 #+15
+elif (idade <= 74):
+    tMax = 260 #+15
+elif (idade <= 79):
+    tMax = 275 #+15
 else:
-    if ((primeira == "N") and (meses < 2) and (ultimas >= 4)):
-        ok = False 
+    tMax = 290 
 
-print(f"Peso: {peso}")
-print(f"Idade: {idade}")
-if (doc != "-"): 
-    print(f"Documento de autorizacao: {doc}")
-print(f"Boa saude: {saude}")
-print(f"Uso drogas injetaveis: {drogas}")
-print(f"Primeira doacao: {primeira}")
-if (primeira == "N"):
-    print(f"Meses desde ultima doacao: {meses}")
-    print(f"Doacoes nos ultimos 12 meses: {ultimas}")
-print(f"Sexo biologico: {sexob}")
-if (sexob == "F"):
-    print(f"Gravidez: {gravida}")
-    print(f"Amamentando: {amamentando}")
-    if(amamentando == "S"):
-        print(f"Meses bebe: {idadebb}")
+print(tMax)
 
-if (ok):
-    print("Procure um hemocentro.")
+f = lambda idade, i, m: (m * (idade//5 - i//5))
+
+if idade < 34:
+    tMax = 180
+elif idade >= 80:
+    tMax = 290
+elif idade <= 54:
+    tMax = (180 if idade <= 44 else 180 + 5) + (f(idade, 34, 5))
 else:
-    if (peso < 50):
-        print("Impedimento: abaixo do peso minimo.")
-    if (16 > idade):
-        print("Impedimento: menor de 16 anos.")
-    elif (idade > 60 and primeira == "S"):
-        print("Impedimento: maior de 60 anos, primeira doacao.")
-    elif (doc != "-" and doc == "N"):
-        print("Impedimento: menor de 18 anos, sem consentimento dos responsaveis.")
-    elif (idade > 69):
-        print("Impedimento: maior de 69 anos.")
-    if (saude == "N"):
-        print("Impedimento: nao esta em boa saude.")
-    if (drogas == "S"):
-        print("Impedimento: uso de drogas injetaveis.")
-    if ((primeira == "N") and ((sexob == "M" and meses < 2) or (sexob == "F" and meses < 3))):
-        print("Impedimento: intervalo minimo entre as doacoes nao foi respeitado.")
-    if ((primeira == "N") and ((sexob == "M" and ultimas >= 2) or (sexob == "F" and ultimas >= 3))):
-        print("Impedimento: numero maximo de doacoes anuais foi atingido.")
-    if ((sexob == "F") and (gravida == "S")):
-        print("Impedimento: gravidez.")
-    if ((amamentando == "S") and (idadebb < 12)):
-        print("Impedimento: amamentacao.")
+    tMax = 215 + (f(idade, 59, 15))
+
+print(tMax)
