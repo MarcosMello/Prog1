fee487992d58f95ae7830fdd13414eb89b9abb86
MarcosMello <Marcosvinicius-vm@outlook.com>
2022-01-15 14:43:22 -0300

    Modified Recusao-TesteAleatorio.py a little.

--- AtividadeApostilas/Recursao-TesteAleatorio.py
+++ AtividadeApostilas/Recursao-TesteAleatorio.py
@@ -9,7 +9,7 @@ def main():
     x = int(input("Insira um valor: "))
     if x < 0:
         x = rec()
-        print(f"Agora sim Ok, {x}")
+        print(f"Agora sim! Ok, {x}")
     else:
         print(f"Ok, {x}")
 
