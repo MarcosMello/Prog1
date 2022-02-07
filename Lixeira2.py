b4cbdb9a9f4f288a41c8f6a6df4e5c6a14ff9f2a
MarcosMello <Marcosvinicius-vm@outlook.com>
2022-02-07 14:42:55 -0300

    Lixeira things

--- Lixeira2.py
+++ Lixeira2.py
@@ -17,10 +17,25 @@ def f1(n):
 print(f(10))
 print(f1(10))"""
 
-a = 1
+"""a = 1
 t = 1
 f = 1
 ar = 1
 
 
-print(True if (a and t and f and ar) else False)
+print(True if (a and t and f and ar) else False)"""
+
+# a = None
+# if (a != None) and (a >= 10):
+#     print("a") 
+
+# from types import NoneType
+
+
+# a = lambda x, y: x - y if x else None
+
+# print(a(-0, -3))
+# print(isinstance(None, NoneType))
+# print(len("int"))
+
+print(0%2)
